f455ba0d4155c829c1a9e594541f32481af395fd
jordin <jordinmathew.mec@gmail.com>
2024-11-20 16:03:04 +1000

    serving html files

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const express = require("express");
+const path = require("path");
+const rootDir=require('./util/path');
+
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 const bodyParser=require('body-parser');
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">submit</button></form>'
-  );
-});
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-app.use("/", (req, res, next) => {
-  res.send('<h1>HI<h1>');
-});
 
+app.use((req,res,next)=>{
+  res.status(404).sendFile(path.join(rootDir,'views','error.html'));
+});
 app.listen(3000);
